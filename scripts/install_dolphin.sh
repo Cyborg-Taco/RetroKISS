382fada41855366ef62758bf5dda914007a0150d
2 Eyed Nerd <dudewithmenot@gmail.com>
2026-01-14 17:15:22 -0600

    Update install_dolphin.sh

--- scripts/install_dolphin.sh
+++ scripts/install_dolphin.sh
@@ -1,5 +1,3 @@
-curl -O https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/scriptmodules/emulators/dolphin.sh
-chmod +x dolphin.sh
 cd scriptmodules/emulators
 chmod +x dolphin.sh
 sudo ./ dolphin.sh
